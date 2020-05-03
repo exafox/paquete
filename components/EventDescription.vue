423a26b07640150302f01a412b7a53b610dd12af
Jon Jandoc <jon.jandoc@gmail.com>
2020-05-03 13:49:07 -0700

    Update description styling

--- components/EventDescription.vue
+++ components/EventDescription.vue
@@ -3,16 +3,16 @@
     class="event-description flex flex-col flex-grow items-start justify-center"
   >
     <template v-if="event">
-      <div class="font-bold leading-none mb-4 text-3xl md:text-5xl font-bold">
+      <div class="font-bold leading-none mb-2 text-3xl md:text-5xl font-bold">
         {{ event.title }}
       </div>
       <div>
         {{ formatDate(event.startTime) }} - {{ formatDate(event.endTime) }}
       </div>
-      <div v-if="event.description" class="mt-2 text-lg">
+      <EventLinks class="mt-4" :event="event" size="lg" />
+      <div v-if="event.description" class="mt-4 text-lg">
         {{ event.description }}
       </div>
-      <EventLinks class="mt-8" :event="event" size="lg" />
     </template>
   </div>
 </template>
