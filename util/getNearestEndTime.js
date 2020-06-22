db94f3fd2dfaa4cc131cde44caf5eae51ab33ff7
Jon Jandoc <jon.jandoc@gmail.com>
2020-06-21 17:24:12 -0700

    Update time rounding functions to allow quarter hour increments

--- util/getNearestEndTime.js
+++ util/getNearestEndTime.js
@@ -2,17 +2,28 @@ import getHours from 'date-fns/getHours';
 import getMinutes from 'date-fns/getMinutes';
 
 /**
- * Returns a time rounded up to the nearest half hour block.
+ * Returns a time rounded up to the nearest quarter hour block.
  * @param {Date} endTime
  * @returns {Date}
  */
-const getNearestEndTime = (endTime) => {
+const getNearestEndTime = (endTime, toHalfHour = false) => {
   const time = new Date(endTime);
   const minutes = getMinutes(time);
   if (minutes > 0) {
+    time.setMinutes(toHalfHour ? 30 : 15);
+  }
+  if (minutes > 15) {
     time.setMinutes(30);
   }
   if (minutes > 30) {
+    if (toHalfHour) {
+      time.setMinutes(0);
+      time.setHours(getHours(time) + 1);
+    } else {
+      time.setMinutes(45);
+    }
+  }
+  if (minutes > 45) {
     time.setMinutes(0);
     time.setHours(getHours(time) + 1);
   }
--- util/getNearestStartTime.js
+++ util/getNearestStartTime.js
@@ -1,18 +1,24 @@
 import getMinutes from 'date-fns/getMinutes';
 
 /**
- * Returns a time rounded down to the nearest half hour block.
+ * Returns a time rounded down to the nearest quarter half hour block.
  * @param {Date} startTime
  * @returns {Date}
  */
-const getNearestStartTime = (startTime) => {
+const getNearestStartTime = (startTime, toHalfHour = false) => {
   const time = new Date(startTime);
   const minutes = getMinutes(time);
   if (minutes >= 0) {
     time.setMinutes(0);
   }
+  if (minutes >= 15 && !toHalfHour) {
+    time.setMinutes(15);
+  }
   if (minutes >= 30) {
     time.setMinutes(30);
+  }
+  if (minutes >= 45 && !toHalfHour) {
+    time.setMinutes(45);
   }
   return time;
 };
