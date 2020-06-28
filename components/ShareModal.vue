7195f276387b118d8caa0a686deeaf05c2035d5b
Mainor Claros <m_claros@live.com>
2020-06-27 22:20:05 -0700

    use grid to equalize share button widths

--- components/ShareModal.vue
+++ components/ShareModal.vue
@@ -1,13 +1,16 @@
 <template>
   <ModalWindow @close="$emit('close')">
     <h1 class="font-bold mb-4 text-xl">Share with:</h1>
-    <ul class="flex flex-col md:flex-row -mx-1">
-      <li v-for="item in shareLinks" :key="item.name" class="m-1">
+    <!-- Note: grid-flow-col will overflow container if number
+    of shareLinks gets long -->
+    <ul class="grid gap-2 md:grid-flow-col">
+      <li v-for="item in shareLinks" :key="item.name">
         <BaseButton
           tag="a"
           :href="item.link"
           target="_blank"
           rel="noopener noreferrer"
+          block
           @click.prevent="trackClick"
         >
           {{ item.name }}
