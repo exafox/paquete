41b013cd4e0bf4449fe48f97700d5216d99ceee7
Mainor Claros <m_claros@live.com>
2020-06-28 12:45:41 -0700

    add api calls to fetch latest channel upload

--- services/youtubeApi.js
+++ services/youtubeApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import get from 'lodash/get';
 
 const apiKey = process.env.GOOGLE_API_KEY;
-const baseURL = 'https://www.googleapis.com/youtube/v3/videos';
+const baseURL = 'https://www.googleapis.com/youtube/v3';
 const headers = {
   'X-Requested-With': 'XMLHttpRequest',
   'Content-Type': 'application/json',
@@ -13,13 +13,35 @@ const api = axios.create({
   headers,
 });
 
+export const fetchChannelLatestUploads = async (channelUsername) => {
+  const { data } = await api.get(
+    `/channels?key=${apiKey}&part=contentDetails&forUsername=${channelUsername}`
+  );
+  return data;
+};
+
+const BOSTON_GLOBE_UPLOADS_PLAYLIST = 'UUcNkwfTQuXAxAFwoAUHweJA';
+export const fetchLatestPlaylistVideos = async (
+  playlistId = BOSTON_GLOBE_UPLOADS_PLAYLIST,
+  maxResults = 1
+) => {
+  const { data } = await api.get(
+    `/playlistItems?key=${apiKey}&playlistId=${playlistId}` +
+      `&maxResults=${maxResults}&part=snippet,contentDetails` +
+      `&fields=items(contentDetails(videoId),snippet(description,title))`
+  );
+  return get(data, 'items', []);
+};
+
 export const fetchEmbeddableStatus = async (videoId) => {
   const { data } = await api.get(
-    `?id=${videoId}&key=${apiKey}&part=status&fields=items(status(embeddable))`
+    `/videos?id=${videoId}&key=${apiKey}&part=status&fields=items(status(embeddable))`
   );
   return get(data, 'items.0.status.embeddable', false);
 };
 
 export default {
+  fetchChannelLatestUploads,
   fetchEmbeddableStatus,
+  fetchLatestPlaylistVideos,
 };
